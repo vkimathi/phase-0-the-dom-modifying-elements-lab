9b33ac38bb9975588cce1d5e3a5b3b38d50cdb77
Victor Kimathi <vkimathi97@gmail.com>
2025-03-30 01:40:00 +0300

    the-dom-modifying-elements-lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main').remove()
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = 'Victor is the champion'
+document.body.append(newHeader)
